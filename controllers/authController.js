ea87d66c90df60435905fdced0ce058d12b1e0b1
manhhoang <hoangmanh09it@gmail.com>
2018-06-27 13:07:46 +0700

    [InviteApp] fixbug run unit test

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,35 +11,44 @@ import jwt from 'jsonwebtoken'
  */
 
 const LOGIN = (req, res) => {
-  User.findOne({
-    where: {
-      username: req.body.username,
-      password: crypto.createHash('sha1').update(req.body.password).digest('hex')
-    }
-  }).then(user => {
-    if (!user) {
-      res.status(401)
+  try {
+    User.findOne({
+      where: {
+        username: req.body.username,
+        password: crypto.createHash('sha1').update(req.body.password).digest('hex')
+      }
+    }).then(user => {
+      if (!user) {
+        res.status(401)
+        res.send({
+          status: 401,
+          statusText: 'Incorrect username or password'
+        })
+        return
+      }
+      let responseData = {
+        id: user.id,
+        username: user.username,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt
+      }
+      responseData.token = jwt.sign(responseData, process.env.SECRET_KEY)
       res.send({
-        status: 401,
-        statusText: 'Incorrect username or password'
+        status: 200,
+        statusText: 'Login sucess',
+        data: responseData
       })
-      return
-    }
-    let responseData = {
-      id: user.id,
-      username: user.username,
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt
-    }
-    responseData.token = jwt.sign(responseData, process.env.SECRET_KEY)
+    }).catch(err => (
+      res.send(err)
+    ))
+  } catch (error) {
+    res.status(500)
     res.send({
-      status: 200,
-      statusText: 'Login sucess',
-      data: responseData
+      status: 500,
+      statusText: 'Have an error in server',
+      err: error
     })
-  }).catch(err => (
-    res.send(err)
-  ))
+  }
 }
 
 const LOGOUT = (req, res) => {
